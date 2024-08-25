fb9ceb62c76622a31d3af21d7bf7bc788f97448f
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-25 14:07:49 +0200

    Auth laytout fuflly

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,75 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "../OAuth";
+
+const SingUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create your Account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your Email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your Password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          {/* OAuth */}
+          <OAuth />
+
+          <Link
+            href="/(auth)/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already Have an account? </Text>
+            <Text className="text-primary-500">Login</Text>
+          </Link>
+          {/* Verificaation Modal */}
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SingUp;
