0a0a1d743a9751defa4dcfd19e18eab954d3e8b7
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-28 15:48:39 +0200

    Payment integration frontend

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 import { PaymentProps } from "@/types/type";
 import CustomButton from "./CustomButton";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { fetchAPI } from "@/lib/fetch";
 
 const Payment = ({
   amount,
@@ -10,7 +12,80 @@ const Payment = ({
   fullName,
   rideTime,
 }: PaymentProps) => {
-  const openPaymentSheet = async () => {};
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+
+  const confirmHandler = async (paymentMethod, _, intentCreationCallback) => {
+    // Make a request to your own server.
+    const { paymentIntent, customer } = await fetchAPI(
+      "(api)/(stripe)/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullName || email.split("@")[0],
+          email: email,
+          amount: amount,
+          paymentMethodId: paymentMethod.id,
+        }),
+      }
+    );
+    if (paymentIntent.client_secret) {
+      const { result } = await fetchAPI("(api)/(stripe)/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intend_id: paymentIntent.id,
+          customer_id: customer,
+        }),
+      });
+      if (result.client_secret) {
+        // API call
+      }
+    }
+
+    // Call the `intentCreationCallback` with your server response's client secret or error
+    const { clientSecret, error } = await response.json();
+    if (clientSecret) {
+      intentCreationCallback({ clientSecret });
+    } else {
+      intentCreationCallback({ error });
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      // Customer canceled - you should probably do nothing.
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      // PaymentSheet encountered an unrecoverable error. You can display the error to the user, log it, etc.
+      setSuccess(true);
+    }
+  };
   return (
     <>
       <CustomButton
