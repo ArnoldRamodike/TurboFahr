62c31bb8237043289290b42945b367ab912ddecd
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-30 16:24:35 +0200

    Fxing frivers error

--- app/(api)/driver+api.ts
+++ app/(api)/driver+api.ts
@@ -1,19 +1,20 @@
 import { neon } from '@neondatabase/serverless';
 
-export async function POST(){
+export async function GET(){
     try {
-    const sql = neon( `${process.env.DATABASE_URL}`);
-
-
-    const response = await sql`SELECT * FROM drivers`;
-
-    return new Response(JSON.stringify({data: response}),{
-        status: 201,
-    })
+    const sql =  neon(`${process.env.DATABASE_URL}`);
+   
+    console.log("SQL: ", sql);
     
+    const drivers =  await sql`SELECT * FROM drivers`;
+
+
+    return new Response(JSON.stringify({data: drivers}),{
+        status: 201,
+    });
         
     } catch (error) {
-        console.log("DRIVER:",error);
+        console.log("DRIVER:", error);
         return Response.json(
             {error: error},
             {status: 500}
